b4482a60692652b11fb4d744eb9326f964665bf1
chiranjivnit <chiranjiv.nit@gmail.com>
2019-02-27 23:05:16 +0530

    Added text to app screen

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Welcome to github.
           </a>
         </header>
       </div>
